cbba8a619fc1c6e20c29dbdca58178ce3b2e1329
YongNyeo <won05121@gmail.com>
2023-08-18 16:53:17 +0900

    feat: add configuration file

---
+++ src/main/java/com/example/norza/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.norza.config;
+
+import com.example.norza.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
