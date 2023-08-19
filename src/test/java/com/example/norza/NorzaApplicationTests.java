9e468ffceb8d2aea0ef6b2e41220e1b0ef4b9481
YongNyeo <won05121@gmail.com>
2023-08-19 22:48:39 +0900

    feat: init file

---
+++ src/test/java/com/example/norza/NorzaApplicationTests.java
@@ -0,0 +1,29 @@
+package com.example.norza;
+
+import com.example.norza.domain.Festival;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class NorzaApplicationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void contextLoads() throws Exception {
+		if (applicationContext != null) {
+			String[] beans = applicationContext.getBeanDefinitionNames();
+
+			for (String bean : beans) {
+				System.out.println("bean : " + bean);
+			}
+		}
+	}
+
+
+
+
+}
